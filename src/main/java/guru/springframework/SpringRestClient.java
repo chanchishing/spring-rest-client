6ba0aec2afdfaf1d11a2732e9aa014f019d19c66
chanchishing <chishing@netvigator.com>
2022-04-22 17:12:33 +0800

    Initial commit

---
+++ src/main/java/guru/springframework/SpringRestClient.java
@@ -0,0 +1,13 @@
+package guru.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestClient {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestClient.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/SpringRestClientTests.java
@@ -0,0 +1,14 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestClientTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
