e128a3e052c197c229f8d3bb2499a70efe41b740
chanchishing <chishing@netvigator.com>
2022-04-25 10:48:36 +0800

    Commit for getting user list from https://jsonplaceholder.typicode.com/users using REST Get

--- src/main/java/guru/springframework/api/domain/Geo.java
+++ src/main/java/guru/springframework/api/domain/Geo.java
@@ -1,42 +1,27 @@
 
 package guru.springframework.api.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 import javax.annotation.Generated;
 
 
+@Builder
+@Getter
+@Setter
 public class Geo implements Serializable
 {
 
     private String lat;
     private String lng;
+    @Builder.Default
     private Map<String, Object> additionalProperties = new HashMap<String, Object>();
     private final static long serialVersionUID = -5390106342352257127L;
 
-    public String getLat() {
-        return lat;
-    }
-
-    public void setLat(String lat) {
-        this.lat = lat;
-    }
-
-    public String getLng() {
-        return lng;
-    }
-
-    public void setLng(String lng) {
-        this.lng = lng;
-    }
-
-    public Map<String, Object> getAdditionalProperties() {
-        return this.additionalProperties;
-    }
-
-    public void setAdditionalProperty(String name, Object value) {
-        this.additionalProperties.put(name, value);
-    }
 
 }
---
+++ src/main/java/guru/springframework/services/ApiService.java
@@ -0,0 +1,10 @@
+package guru.springframework.services;
+
+import guru.springframework.api.domain.User;
+
+import java.util.List;
+
+public interface ApiService {
+
+    public List<User> getUsers(Integer limit);
+}
