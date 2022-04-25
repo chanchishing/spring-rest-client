1d228206b304918159f961a577110e72d849ce13
chanchishing <chishing@netvigator.com>
2022-04-25 12:02:37 +0800

    Move the URL https://jsonplaceholder.typicode.com/users to application.properties for cleaner code

--- src/main/java/guru/springframework/services/ApiServiceImpl.java
+++ src/main/java/guru/springframework/services/ApiServiceImpl.java
@@ -1,9 +1,13 @@
 package guru.springframework.services;
 
 import guru.springframework.api.domain.User;
-import guru.springframework.config.RestTemplateConfig;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -11,14 +15,23 @@ import java.util.List;
 public class ApiServiceImpl implements ApiService{
 
     RestTemplate restTemplate;
+    private final String apiURL;
 
-    public ApiServiceImpl(RestTemplate restTemplate) {
+
+
+    public ApiServiceImpl(RestTemplate restTemplate, @Value("${api.url}") String apiURL) {
         this.restTemplate = restTemplate;
+        this.apiURL=apiURL;
     }
 
     @Override
     public List<User> getUsers(Integer limit) {
-        List<User> userData = restTemplate.getForObject("https://jsonplaceholder.typicode.com/users?_limit=" + limit, List.class);
+
+        UriComponentsBuilder uriBuilder=UriComponentsBuilder
+                .fromUriString(apiURL)
+                .queryParam("_limit",limit);
+
+        List<User> userData = restTemplate.getForObject(uriBuilder.toUriString(), List.class);
 
         return userData;
     }
